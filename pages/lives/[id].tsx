beba412093fd14bfcba25c1f2bfda37801363167
baekjung <zvgandam@gmail.com>
2022-04-25 15:29:23 +0900

    add stream

--- pages/lives/[id].tsx
+++ pages/lives/[id].tsx
@@ -4,10 +4,22 @@ const LiveDetail: NextPage = () => {
   return (
     <div className="space-y-4 px-4 py-10">
       <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
-      <h3 className="mt-2 text-2xl font-semibold text-gray-800">
-        Let&apos;s try potatos
-      </h3>
-      <div className="h-[50vh] space-y-4 overflow-y-scroll py-10 px-4 pb-16">
+      <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
+      <span className="mt-3 block text-2xl text-gray-900">$140</span>
+      <p className="my-6 text-base text-gray-700">
+        My money&apos;s in that office, right? If she start giving me some
+        bullshit about it ain&apos;t there, and we got to go someplace else and
+        get it, I&apos;m gonna shoot you in the head then and there. Then
+        I&apos;m gonna shoot that bitch in the kneecaps, find out where my
+        goddamn money is. She gonna tell me too. Hey, look at me when I&apos;m
+        talking to you, motherfucker. You listen: we go in there, and that ni**a
+        Winston or anybody else is in there, you the first motherfucker to get
+        shot. You understand?
+      </p>
+      <span className="mt-2 block text-2xl font-semibold text-gray-900">
+        Live Chat
+      </span>
+      <div className="h-[50vh] space-y-4 overflow-y-scroll py-4 px-4 pb-16">
         <div className="flex items-start space-x-2">
           <div className="h-8 w-8 rounded-full bg-slate-400" />
           <div className="w-1/2 rounded-md border border-gray-300 p-2 text-sm text-gray-700">
