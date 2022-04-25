cf9485ea663fccbbb370670beb7bf59432060148
baekjung <zvgandam@gmail.com>
2022-04-25 12:46:57 +0900

    profile bought, favorite, sold

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -165,14 +165,14 @@ const ChatDetail: NextPage = () => {
           <p>미쳤어</p>
         </div>
       </div>
-      <div className="fixed inset-x-0 bottom-2 left-0 right-0 mx-auto w-full max-w-md">
-        <div className="relative flex items-center">
+      <div className="fixed inset-x-0 bottom-0 bg-white py-2">
+        <div className="relative mx-auto flex w-full max-w-md items-center">
           <input
             type="text"
             className="w-full rounded-full border-gray-300 pr-12 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
           />
           <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
-            <button className="flex cursor-pointer items-center rounded-full bg-orange-500 px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
+            <button className="flex items-center rounded-full bg-orange-500 px-3 text-sm text-white hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
               &rarr;
             </button>
           </div>
