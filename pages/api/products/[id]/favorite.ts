f24438b2450ddb6bc04002a03df1e214f9683938
baekjung <zvgandam@gmail.com>
2022-05-17 17:03:02 +0900

    favorite product

---
+++ pages/api/products/[id]/favorite.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+
+  const product = await client.product.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+  });
+
+  if (!product) {
+    return res.json({ ok: false, message: "Not found product" });
+  }
+
+  const alreadyExists = await client.favorite.findFirst({
+    where: {
+      productId: product.id,
+      userId: user?.id,
+    },
+  });
+
+  if (alreadyExists) {
+    await client.favorite.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    await client.favorite.create({
+      data: {
+        product: {
+          connect: {
+            id: product.id,
+          },
+        },
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+  }
+
+  res.json({ ok: true });
+}
+
+export default withApiSession(withHandler({ methods: ["POST"], handler }));
