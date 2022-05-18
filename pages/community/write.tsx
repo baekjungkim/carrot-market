d1ea6a863c72b5c76908098eddb47d9cd04548c2
baekjung <zvgandam@gmail.com>
2022-05-18 17:55:49 +0900

    Post Forms and Handlers

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -2,13 +2,46 @@ import { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import { useForm } from "react-hook-form";
+import useMutation from "@libs/client/useMutation";
+import { useEffect } from "react";
+import { Post } from "@prisma/client";
+import { useRouter } from "next/router";
+
+interface WriteForm {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [writePost, { loading, data }] =
+    useMutation<WriteResponse>("/api/posts");
+  const onValid = (data: WriteForm) => {
+    if (loading) return;
+    writePost(data);
+  };
+  useEffect(() => {
+    if (data?.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data, router]);
+
   return (
     <Layout title="질문 등록" isGoBack>
-      <form className="space-y-4 p-4">
-        <TextArea required placeholder="Ask a question!" />
-        <Button text="Save" />
+      <form className="space-y-4 p-4" onSubmit={handleSubmit(onValid)}>
+        <TextArea
+          register={register("question", { required: true, minLength: 5 })}
+          required
+          placeholder="Ask a question!"
+          disabled={loading}
+        />
+        <Button text={loading ? "Loading..." : "Submit"} />
       </form>
     </Layout>
   );
