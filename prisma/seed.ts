d707d3b4a03f4789b49af3c34cc471542056109a
baekjung <zvgandam@gmail.com>
2022-05-23 17:24:08 +0900

    seeding

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { clearTimeout } from "timers";
+
+const client = new PrismaClient();
+
+const main = async () => {
+  [...Array.from(Array(500).keys())].forEach(async (item) => {
+    await client.stream.create({
+      data: {
+        name: String(item),
+        description: String(item),
+        price: item,
+        user: {
+          connect: {
+            id: 11,
+          },
+        },
+      },
+    });
+  });
+};
+
+main()
+  .catch((e) => console.log(e))
+  .finally(() => client.$disconnect());
